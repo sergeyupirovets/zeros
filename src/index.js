a978a799c2d7619a2403bda8c90dac36221718fb
Sergey Upirovets <sergey1b.u@gmail.com>
2018-03-04 13:47:04 +0300

    zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var counter = 0;
+  for (var i = 5; number/i >= 1; i *= 5)
+    counter += Math.floor(number/i);
+  return counter;
 }
